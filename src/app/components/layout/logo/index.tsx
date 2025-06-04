9b865a768495346fcb0d1b44f84b772beb7e9477
Nils Hansson <nilsmenghansson@gmail.com>
2025-06-04 19:19:29 +0200

    fixat logo

--- src/app/components/layout/logo/index.tsx
+++ src/app/components/layout/logo/index.tsx
@@ -1,23 +1,35 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 const Logo = (props: { sticky: boolean }) => {
-    const { sticky } = props;
-    return (
-        <Link href="/">
-            <Image
-                src={sticky ? "/images/logo/sticky_logo.svg" : "/images/logo/WhiteLogo.svg"}
-                alt="logo"
-                width={190}
-                height={34}
-                style={{ width: 'auto', height: 'auto' }}
-                quality={100}
-                priority={true}
-                className='hidden xsm:block'
-            />
-            <Image src={sticky ? "/images/logo/favicondark.svg" : "/images/logo/favicon.svg"} alt='logo' width={40} height={40} className='block xsm:hidden' />
-        </Link>
-    );
+  const { sticky } = props;
+  return (
+    <Link href="/">
+      <Image
+        src={
+          sticky
+            ? "/images/logo/signatur-utan-bakgrund.png"
+            : "/images/logo/signatur-black.png"
+        }
+        alt="logo"
+        width={190}
+        height={34}
+        style={{ width: "80px", height: "auto" }}
+        quality={100}
+        priority={true}
+        className="hidden xsm:block"
+      />
+      <Image
+        src={
+          sticky ? "/images/logo/favicondark.svg" : "/images/logo/favicon.svg"
+        }
+        alt="logo"
+        width={40}
+        height={40}
+        className="block xsm:hidden"
+      />
+    </Link>
+  );
 };
 
 export default Logo;
--- src/app/components/scroll-to-top/index.tsx
+++ src/app/components/scroll-to-top/index.tsx
@@ -28,16 +28,18 @@ export default function ScrollToTop() {
   return (
     <div className="fixed bottom-8 right-8 z-[999]">
       <div className="flex items-center gap-2.5">
-        <Link href={"https://getnextjstemplates.com/products/endeavor-pro-nextjs-charity-website-template"} target="_blank" className="hidden lg:block text-sm bg-primary hover:bg-primary/70 text-secondary font-semibold px-4 py-3.5 leading-none rounded-lg duration-300 text-nowrap">
-          Get This Template
-        </Link>
         {isVisible && (
           <div
             onClick={scrollToTop}
             aria-label="scroll to top"
             className="flex cursor-pointer items-center justify-center transition duration-300 ease-in-out"
           >
-            <Image src={"/images/Icon/up-arrow.svg"} alt="Images" width={55} height={55} />
+            <Image
+              src={"/images/Icon/up-arrow.svg"}
+              alt="Images"
+              width={55}
+              height={55}
+            />
           </div>
         )}
       </div>
