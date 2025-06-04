512c64c6980faa27d65aa42a60f66c99554321ab
Nils Hansson <nilsmenghansson@gmail.com>
2025-06-04 19:25:45 +0200

    removed stats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,28 +7,27 @@ import Portfolio from "./components/home/portfolio";
 import Pricing from "./components/home/pricing";
 import Resources from "./components/home/resources";
 import Services from "./components/home/services";
-import StatsFacts from "./components/home/stats-facts";
+
 import Team from "./components/home/team";
 import Testimonial from "./components/home/testimonial";
 
 export const metadata: Metadata = {
-    title: "Studiova",
+  title: "Studiova",
 };
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      <StatsFacts/>
-      <Portfolio/>
-      <Services/>
-      <Aboutus/>
-      <Testimonial/>
-      <Team teamdataNumber="06"/>
-      <Pricing/>
-      <Faq/>
-      <Resources/>
-      <Contact contactdataNumber="10"/>
+      <Portfolio />
+      <Services />
+      <Aboutus />
+      <Testimonial />
+      <Team teamdataNumber="06" />
+      <Pricing />
+      <Faq />
+      <Resources />
+      <Contact contactdataNumber="10" />
     </>
   );
 }
