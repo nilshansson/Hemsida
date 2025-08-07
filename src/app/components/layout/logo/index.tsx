dc617a4a46c9025a070a9016f9408ea0d9333c12
Nils Hansson <nilsmenghansson@gmail.com>
2025-08-07 20:28:02 +0200

    provar mindre logo på mobilen

--- src/app/components/layout/logo/index.tsx
+++ src/app/components/layout/logo/index.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 
 const Logo = (props: { sticky: boolean }) => {
   const { sticky } = props;
+
   return (
-    <Link href="/">
+    <Link href="/" className="flex items-center">
+      {/* Signature logo – shown on larger screens */}
       <Image
         src={
           sticky
@@ -14,19 +16,20 @@ const Logo = (props: { sticky: boolean }) => {
         alt="logo"
         width={190}
         height={34}
-        style={{ width: "80px", height: "auto" }}
         quality={100}
         priority={true}
-        className="hidden xsm:block"
+        className="hidden xsm:block w-[100px] md:w-[140px] lg:w-[190px] h-auto"
       />
+
+      {/* Favicon logo – shown on extra small screens */}
       <Image
         src={
           sticky ? "/images/logo/favicondark.svg" : "/images/logo/favicon.svg"
         }
         alt="logo"
-        width={100} // increased from 100
-        height={40} // increased from 40
-        className="block xsm:hidden"
+        width={100}
+        height={40}
+        className="block xsm:hidden w-[40px] h-auto"
       />
     </Link>
   );
